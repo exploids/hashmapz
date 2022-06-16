9573e2df69b92abbac68697070ab8ee1a5a7b31b
Niklas <niklas.rubel@study.hs-duesseldorf.de>
2022-06-16 21:38:57 +0200

    calculateStepsAdd() modfied and all doCommand() methods written

---
+++ app/src/main/java/com/exploids/hashmapz/command/GoToIndexCommand.kt
@@ -0,0 +1,13 @@
+package com.exploids.hashmapz.command
+
+import com.exploids.hashmapz.model.CurrentState
+
+class GoToIndexCommand : Command {
+    override fun doCommand(state: CurrentState) {
+        state.currentIndex = state.usedIndex
+    }
+
+    override fun undoCommand(state: CurrentState) {
+        TODO("Not yet implemented")
+    }
+}
