17599d04fc36153f22cb9bfdc394f5382fce614f
Luca Selinski <lucaselinski@gmail.com>
2022-07-02 16:36:59 +0200

    Implemented tutorial foundation

--- app/src/main/java/com/exploids/hashmapz/TutorialActivity.kt
+++ app/src/main/java/com/exploids/hashmapz/TutorialActivity.kt
@@ -1,21 +1,30 @@
 package com.exploids.hashmapz
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.exploids.hashmapz.ui.components.NestedScaffold
+import com.exploids.hashmapz.ui.onboarding.OnboardingView
 import com.exploids.hashmapz.ui.theme.HashmapzTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class,
+    ExperimentalPagerApi::class
+)
 @Composable
 fun Tutorial(navController: NavController) {
-    NestedScaffold(navController = navController, title = "Tutorial") {
+    Scaffold {
         Column(modifier = Modifier.padding(it)) {
+            OnboardingView {
+                navController.navigate("home")
+            }
         }
     }
 }
