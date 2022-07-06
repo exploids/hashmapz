158e724a7229c1289a82f069f38f8d99ce39f80a
Natapata <83461493+Natapata@users.noreply.github.com>
2022-07-06 13:58:53 +0200

    Update WikiActivity.kt
    text

--- app/src/main/java/com/exploids/hashmapz/WikiActivity.kt
+++ app/src/main/java/com/exploids/hashmapz/WikiActivity.kt
@@ -1,5 +1,6 @@
 package com.exploids.hashmapz
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,12 +18,17 @@ import com.exploids.hashmapz.ui.theme.HashmapzTheme
 @Composable
 fun Wiki(navController: NavController) {
     NestedScaffold(navController = navController, title = "Wiki") {
-        Column(modifier = Modifier.padding(it)) {
-            Text(text = "HashmapZ", style = MaterialTheme.typography.titleLarge)
-            Text(
-                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam at lectus urna duis convallis convallis tellus id interdum. Mauris ultrices eros in cursus turpis massa tincidunt. Odio euismod lacinia at quis risus sed vulputate odio. Gravida rutrum quisque non tellus. Purus faucibus ornare suspendisse sed nisi lacus sed viverra. Sed lectus vestibulum mattis ullamcorper velit sed. Semper feugiat nibh sed pulvinar. Mi eget mauris pharetra et. Nunc lobortis mattis aliquam faucibus. Aliquam etiam erat velit scelerisque in. Eu mi bibendum neque egestas congue quisque egestas. Cursus vitae congue mauris rhoncus aenean vel. Mauris cursus mattis molestie a iaculis at. Bibendum est ultricies integer quis auctor elit sed vulputate. At imperdiet dui accumsan sit.",
-                style = MaterialTheme.typography.bodyMedium
-            )
+        Column(modifier = Modifier.padding(it)
+        ) {
+            Text(text = "What are hash maps?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "A hash map maps a key to a value and stores this value pair in a hash table. The key is hashed and the hash code is used as an index to reference where the value is stored in the table. Also, key and value do not have to be of the same data type, e.g. the key can be of the data type string and the value of the data type integer.",
+                style = MaterialTheme.typography.bodyMedium)
+            Text(text = "What is hashing?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "Hashing is a cryptographic function, the so-called hash function, which works only in one direction. In our case, the value of the key is converted into a hash value by mathematical function. The Hash value cannot be converted into the original value afterward. Hash values will always be the same length, no matter how big the value of the key was before. In addition, different values of the key should always result in a different Hashvalue and equal values to the same Hashvalue.",
+                style = MaterialTheme.typography.bodyMedium)
+            Text(text = "What happens when the table is full?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "The table of the HashMap cannot become completely full unless you change the load factor. Because the load factor indicates how full the table can become before the table is extended automatically. Then the hash table is hashed again, i.e. the internal data structure is formed anew. The rebuilding should approximately double the table.",
+                style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
