996f931654d5ed43d4a971b8257d81cc0e343bec
Natapata <83461493+Natapata@users.noreply.github.com>
2022-07-06 15:20:10 +0200

    Update WikiActivity.kt

--- app/src/main/java/com/exploids/hashmapz/WikiActivity.kt
+++ app/src/main/java/com/exploids/hashmapz/WikiActivity.kt
@@ -24,16 +24,17 @@ fun Wiki(navController: NavController) {
         Column(modifier = Modifier.padding(it).padding(16.dp).verticalScroll(ScrollState(0)),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Text(text = "What are hash maps?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "What are hash maps?", style = MaterialTheme.typography.headlineMedium)
             Text(text = "A hash map maps a key to a value and stores this value pair in a hash table. The key is hashed and the hash code is used as an index to reference where the value is stored in the table. Also, key and value do not have to be of the same data type, e.g. the key can be of the data type string and the value of the data type integer.",
                 style = MaterialTheme.typography.bodyMedium)
-            Text(text = "What is hashing?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "What is hashing?", style = MaterialTheme.typography.headlineMedium)
             Text(text = "Hashing is a cryptographic function, the so-called hash function, which works only in one direction. In our case, the value of the key is converted into a hash value by mathematical function. The Hash value cannot be converted into the original value afterward. Hash values will always be the same length, no matter how big the value of the key was before. In addition, different values of the key should always result in a different Hashvalue and equal values to the same Hashvalue.",
                 style = MaterialTheme.typography.bodyMedium)
-            Text(text = "What happens when the table is full?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "What happens when the table is full?", style = MaterialTheme.typography.headlineMedium)
             Text(text = "The table of the HashMap cannot become completely full unless you change the load factor. Because the load factor indicates how full the table can become before the table is extended automatically. Then the hash table is hashed again, i.e. the internal data structure is formed anew. The rebuilding should approximately double the table.",
                 style = MaterialTheme.typography.bodyMedium)
-            Text(text = "Deleting an entry sometimes restructures the hash map, why?", style = MaterialTheme.typography.titleLarge)
+            Text(text = "Deleting an entry sometimes restructures the hash map, why?",
+                style = MaterialTheme.typography.headlineMedium)
             Text(text = "needs Text!", style = MaterialTheme.typography.bodyMedium)
         }
     }
